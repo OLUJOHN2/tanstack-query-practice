906390bad3803184cbd1075d8c777f3d8c743662
OLUJOHN2 <olumoyepelumi1@gmail.com>
2026-04-03 11:39:45 +0100

    StaleTime

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,15 @@ import React from "react";
 import WithTanstackQuery from "./components/3. WithTanstakQuery";
 import Deduplication from "./components/4. Deduplication";
 import FetchOneItem from "./components/5. FetchOneItem";
+import StaleTime from "./components/6. StaleTime";
 
 const App = () => {
   return (
     <div>
       {/* <WithTanstackQuery /> */}
       {/* <Deduplication /> */}
-      <FetchOneItem />
+      {/* <FetchOneItem /> */}
+      <StaleTime />
     </div>
   );
 };
