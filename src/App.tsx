f3c2451ef991db48aaf1c5b5353851ea199200b1
OLUJOHN2 <olumoyepelumi1@gmail.com>
2026-04-03 12:05:19 +0100

    Fetch from multiple endpoints

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Deduplication from "./components/4. Deduplication";
 import FetchOneItem from "./components/5. FetchOneItem";
 import StaleTime from "./components/6. StaleTime";
 import RefetchInterval from "./components/7. RefetchInterval";
+import FetchFromMultipleEndpoints from "./components/8. FetchFromMultipleEndpoints";
 
 const App = () => {
   return (
@@ -12,7 +13,8 @@ const App = () => {
       {/* <Deduplication /> */}
       {/* <FetchOneItem /> */}
       {/* <StaleTime /> */}
-      <RefetchInterval />
+      {/* <RefetchInterval /> */}
+      <FetchFromMultipleEndpoints />
     </div>
   );
 };
