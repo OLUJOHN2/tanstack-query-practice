1edffae91fc94b395c3d9f91f1a8c8165746e301
OLUJOHN2 <olumoyepelumi1@gmail.com>
2026-04-03 11:47:26 +0100

    RefetchInterval

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import WithTanstackQuery from "./components/3. WithTanstakQuery";
 import Deduplication from "./components/4. Deduplication";
 import FetchOneItem from "./components/5. FetchOneItem";
 import StaleTime from "./components/6. StaleTime";
+import RefetchInterval from "./components/7. RefetchInterval";
 
 const App = () => {
   return (
@@ -10,7 +11,8 @@ const App = () => {
       {/* <WithTanstackQuery /> */}
       {/* <Deduplication /> */}
       {/* <FetchOneItem /> */}
-      <StaleTime />
+      {/* <StaleTime /> */}
+      <RefetchInterval />
     </div>
   );
 };
