f85876fe1e8f815829b170f6fc7a2d9c6ad6a500
OLUJOHN2 <olumoyepelumi1@gmail.com>
2026-04-03 11:28:56 +0100

    fourth commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import WithTanstackQuery from "./components/3. WithTanstakQuery";
 import Deduplication from "./components/4. Deduplication";
+import FetchOneItem from "./components/5. FetchOneItem";
 
 const App = () => {
   return (
     <div>
       {/* <WithTanstackQuery /> */}
-      <Deduplication />
+      {/* <Deduplication /> */}
+      <FetchOneItem />
     </div>
   );
 };
